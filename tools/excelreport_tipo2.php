db343de87abaa420424d1d171a5785992181dfa6
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-26 20:29:31 -0300

    Arreglando entidad producto

--- tools/excelreport_tipo2.php
+++ tools/excelreport_tipo2.php
@@ -42,7 +42,7 @@ class ExcelReportTipo2 extends View {
     
     $objPHPExcel->setActiveSheetIndex(0)
                 ->setShowGridlines(false)
-                //->mergeCells("B1:E1")
+                ->mergeCells("B1:E1")
                 //->mergeCells("F1:{$ultimaLetraPosicion}1")
                 //->setCellValue("B1", $tituloReporte)
                 //->setCellValue("F1", $fechaReporte)
